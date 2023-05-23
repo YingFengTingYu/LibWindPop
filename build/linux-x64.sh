360b05538fb072d7c54003f01b3694aa4f5cecf2
风语 <10127265+with-wind-and-rain@user.noreply.gitee.com>
2023-05-23 10:30:11 +0800

    test for fixed linux publish script

--- build/linux-x64.sh
+++ build/linux-x64.sh
@@ -1,5 +1,7 @@
-dotnet publish ../src/LibWindPop.csproj -c Release -f net8.0 -o ../build/publish/managed
-dotnet publish ../src/LibWindPop.csproj /p:NativeLib=Static\;PublishAot=true -c Release -f net8.0 -r linux-x64 -o ../build/publish/nativeaot/static/linux-x64
-cp ../src/libwindpop.h ../build/publish/nativeaot/static/linux-x64/LibWindPop.h
-dotnet publish ../src/LibWindPop.csproj /p:NativeLib=Shared\;PublishAot=true -c Release -f net8.0 -r linux-x64 -o ../build/publish/nativeaot/shared/linux-x64
-cp ../src/libwindpop.h ../build/publish/nativeaot/shared/linux-x64/LibWindPop.h
+cd ../src
+dotnet publish -c Release -f net8.0 -o ../build/publish/managed
+dotnet publish /p:NativeLib=Static\;PublishAot=true -c Release -f net8.0 -r linux-x64 -o ../build/publish/nativeaot/static/linux-x64
+cp libwindpop.h ../build/publish/nativeaot/static/linux-x64/LibWindPop.h
+dotnet publish /p:NativeLib=Shared\;PublishAot=true -c Release -f net8.0 -r linux-x64 -o ../build/publish/nativeaot/shared/linux-x64
+cp libwindpop.h ../build/publish/nativeaot/shared/linux-x64/LibWindPop.h
+cd ../build
