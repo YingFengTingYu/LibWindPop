bbb5718351cd9e9f8c465442d6ab67cb42d3d2a4
风语 <10127265+with-wind-and-rain@user.noreply.gitee.com>
2023-05-23 19:16:26 +0800

    fixed win build github actions bat

--- build/actions/win-build.bat
+++ build/actions/win-build.bat
@@ -1,6 +1,6 @@
 cd build
-win-x64-shared-library.bat
-win-x64-static-library.bat
-win-arm64-shared-library.bat
-win-arm64-static-library.bat
+call win-x64-shared-library.bat
+call win-x64-static-library.bat
+call win-arm64-shared-library.bat
+call win-arm64-static-library.bat
 cd ..
